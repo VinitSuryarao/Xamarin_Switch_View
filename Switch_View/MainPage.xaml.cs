5c82caa667e64deef09be476e02b1d54f053f30e
Vinit Suryarao <suryarao.vinit@hotmail.com>
2022-09-17 19:03:05 +0530

    First Commit

---
+++ Switch_View/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Switch_View
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SwitchImg_Toggled(object sender, ToggledEventArgs e)
+        {
+            bool value = e.Value;
+            if (value)
+            {
+                BackgroundImageSource = "SampleImg4.jpg";
+            }
+            else
+            {
+                BackgroundImageSource = "SampleImg5.jpg";
+            }
+        }
+    }
+}
